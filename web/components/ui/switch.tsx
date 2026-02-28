5e3531be7afa5136437e97953945717f7a88028b
bimoraa <keceomag@gmail.com>
2026-02-28 18:35:25 +0700

    fix: remove commented-out code in Switch component for cleaner implementation

--- web/components/ui/switch.tsx
+++ web/components/ui/switch.tsx
@@ -19,8 +19,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
-        // - SIZE OVERRIDES FROM PARENT VIA [&>span] ARE APPLIED AFTER - \\
+        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
       )}
     />
   </SwitchPrimitives.Root>
