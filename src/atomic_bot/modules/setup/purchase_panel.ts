2b05dfac145ce544d82b4ae6f5e7a78971116887
bimoraa <keceomag@gmail.com>
2026-03-02 22:34:20 +0700

    feat: enhance purchase panel command to support message editing and improved error handling

--- src/atomic_bot/modules/setup/purchase_panel.ts
+++ src/atomic_bot/modules/setup/purchase_panel.ts
@@ -4,21 +4,28 @@ import {
   TextChannel,
   GuildMember,
 } from "discord.js"
-import { Command } from "@shared/types/command"
-import { is_admin } from "@shared/database/settings/permissions"
-import { component, api } from "@shared/utils"
+import { Command }          from "@shared/types/command"
+import { is_admin }         from "@shared/database/settings/permissions"
+import { component, api }   from "@shared/utils"
+import { log_error }        from "@shared/utils/error_logger"
 import { get_ticket_config } from "@shared/database/unified_ticket"
 
 export const command: Command = {
   data: new SlashCommandBuilder()
     .setName("purchase_panel")
-    .setDescription("Send the purchase ticket panel") as SlashCommandBuilder,
+    .setDescription("Send or edit the purchase ticket panel")
+    .addStringOption((opt) =>
+      opt
+        .setName("message_id")
+        .setDescription("Message ID to edit (leave empty to send new)")
+        .setRequired(false)
+    ) as SlashCommandBuilder,
 
   async execute(interaction: ChatInputCommandInteraction) {
     if (!is_admin(interaction.member as GuildMember)) {
       await interaction.reply({
         content: "You don't have permission to use this command.",
-        flags: 64,
+        flags   : 64,
       })
       return
     }
@@ -51,37 +58,49 @@ export const command: Command = {
           components: [
             component.text([
               "## Purchase Ticket",
-              "If you have questions about the script please use <#1250786601462923396> to ask our staffs.",
-              "",
               "You can only create a ticket to purchase a script. Opening a ticket without making a purchase will be considered intentional trolling and a warning will be given.",
               "",
               "Our script price is stated in <#1250770696876068927>",
             ]),
-            component.divider(),
-            component.section({
-              content: [
-                "## Payment Method:",
-                "<:qris:1251913366713139303> - QRIS ( Quick Response Code Indonesian Standard )",
-                "<:paypal:1251913398816604381> - Paypal",
-                "<:gopay:1251913342646489181> - Gopay",
-                "<:dana:1251913282923790379> - Dana",
-                "<:ovo:1251913316092088404> - Ovo",
-              ],
-              thumbnail: "https://github.com/bimoraa/atomic_bot/blob/main/assets/images/atomic_logo.png?raw=true",
-            }),
-            component.divider(),
+          ],
+        }),
+        component.container({
+          components: [
+            component.text([
+              "## Payment Method:",
+              "- <:qris:1251913366713139303> - QRIS ( Quick Response Code Indonesian Standard )",
+              "- <:dana:1251913282923790379> - Dana",
+              "- <:gopay:1251913342646489181> - Gopay",
+              "-  <:paypal:1251913398816604381> - Paypal",
+            ]),
+          ],
+        }),
+        component.container({
+          components: [
             component.action_row(
-              component.secondary_button("Open", "purchase_open", component.emoji_object("ticket", "1411878131366891580"))
+              component.success_button("Purchase Script ( Open a Ticket )", "__purchase_script")
             ),
           ],
         }),
       ],
     })
 
-    await api.send_components_v2(channel.id, api.get_token(), message)
+    const message_id = interaction.options.getString("message_id")
 
-    await interaction.editReply({
-      content: "Purchase panel sent!",
-    })
+    try {
+      if (message_id) {
+        await api.edit_components_v2(channel.id, message_id, api.get_token(), message)
+        await interaction.editReply({ content: `Purchase panel updated! (message: ${message_id})` })
+      } else {
+        await api.send_components_v2(channel.id, api.get_token(), message)
+        await interaction.editReply({ content: "Purchase panel sent!" })
+      }
+    } catch (err) {
+      await log_error(interaction.client, err as Error, "Purchase Panel Send/Edit", {
+        guild_id  : interaction.guildId  ?? undefined,
+        user_id   : interaction.user.id,
+      }).catch(() => {})
+      await interaction.editReply({ content: "Failed to send/edit purchase panel." })
+    }
   },
 }
